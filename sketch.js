ab48f9c69fddf5c10d2a1cb42ffef26a86b1c85a
Alvin Yan <alvinyan12345@gmail.com>
2023-11-18 15:46:59 -0500

    Add all files

---
+++ sketch.js
@@ -0,0 +1,51 @@
+
+let pX = 125, pY = 125, spd = 7;
+
+function setup(){
+    myCanvas = createCanvas(windowWidth-50, windowHeight-50);
+    myCanvas.parent('canvas');
+    background(0);
+    noStroke();
+    fill(10);
+    rectMode(CENTER);
+}
+
+
+let backgroundImage;
+
+function preload() {
+  // Load the image in the preload function
+  backgroundImage = loadImage('mapCropped.png');
+}
+
+function draw() {
+  // Set the loaded image as the background
+  background(backgroundImage);
+
+  // Draw a red rectangle at (pX, pY)
+  fill('red');
+  rect(pX - 10, pY - 10, 20, 20);
+
+
+    if (keyIsDown(87)||keyIsDown(38)) pY -= spd;
+    if (keyIsDown(83)||keyIsDown(40)) pY += spd;
+    if (keyIsDown(65)||keyIsDown(37)) pX -= spd; 
+    if (keyIsDown(68)||keyIsDown(39)) pX += spd;
+
+    if (pY > windowHeight-50) pY = windowHeight-50;
+    if (pX > windowWidth-50) pX = windowWidth-50;
+    if (pY < 20) pY = 20;
+    if (pX < 20) pX = 20;
+
+}
+
+function keyPressed() {
+    if (keyCode===122) {
+        console.log("test")
+
+    document.getElementById('defaultCanvas0').style.width = "100vw";
+    document.getElementById('defaultCanvas0').style.height = "100vh";
+
+    }
+  }
+
